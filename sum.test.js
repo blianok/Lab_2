d19930915ffc9742b79162081b67ee986df6eb8d
blianok <you@example.com>
2024-10-29 15:35:16 +0300

    update file sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,4 @@
 const sum = require('./sum');
-test('adds 1 + 2 to equal 3', () => {
-	const result = sum(1, 2);
-	console.log("Это сумма запрашиваемого числа:", result);
-	expect(result).toBe(3);	
+test('adds 1 + 2 to equal 3', () => 
+	expect(sum(1, 2)).toBe(3);
 });
